5d189b2f619c120f55717a79f4d65d74d9542d7d
Никита <128186146+n1kkita@users.noreply.github.com>
2023-09-05 18:51:51 +0300

    create CarRepository

--- src/main/java/com/myfirstwebsocketapp/app/controllers/api/CarController.java
+++ src/main/java/com/myfirstwebsocketapp/app/controllers/api/CarController.java
@@ -3,11 +3,13 @@ package com.myfirstwebsocketapp.app.controllers.api;
 import com.myfirstwebsocketapp.app.entity.Car;
 import com.myfirstwebsocketapp.app.services.CarService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@Slf4j
 @RequestMapping("/cars")
 @RequiredArgsConstructor
 public class CarController {
--- src/main/java/com/myfirstwebsocketapp/app/services/imp/CarServiceImpl.java
+++ src/main/java/com/myfirstwebsocketapp/app/services/imp/CarServiceImpl.java
@@ -2,9 +2,17 @@ package com.myfirstwebsocketapp.app.services.imp;
 
 import com.myfirstwebsocketapp.app.dto.CarDto;
 import com.myfirstwebsocketapp.app.entity.Car;
+import com.myfirstwebsocketapp.app.repositories.CarRepository;
 import com.myfirstwebsocketapp.app.services.CarService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class CarServiceImpl implements CarService {
+    private final CarRepository carRepository;
     @Override
     public Car save(CarDto carDto) {
         return null;
