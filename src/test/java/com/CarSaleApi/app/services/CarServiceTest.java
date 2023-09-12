1a80a072cae1f2806b6f80b768ab1fbb97032a73
Никита <128186146+n1kkita@users.noreply.github.com>
2023-09-12 20:16:18 +0300

    optimization of tests for CarService and SellerService

--- src/test/java/com/CarSaleApi/app/services/CarServiceTest.java
+++ src/test/java/com/CarSaleApi/app/services/CarServiceTest.java
@@ -1,46 +1,30 @@
 package com.CarSaleApi.app.services;
 
 
-import com.CarSaleApi.app.dto.CarDto;
-import com.CarSaleApi.app.dto.CarShowroomDto;
+import com.CarSaleApi.app.config.ConfigTestBean;
 import com.CarSaleApi.app.entity.Car;
 import com.CarSaleApi.app.exceptions.CarNotFoundException;
 import com.CarSaleApi.app.exceptions.MachinesOutOfStockException;
-import com.CarSaleApi.app.repositories.CarRepository;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@ContextConfiguration(classes = {ConfigTestBean.class})
 class CarServiceTest {
-
     @Autowired
     private CarService carService;
     @Autowired
-    private CarRepository  carRepository;
-    private Car testCar;
-    @BeforeEach
-    void setup(){
-
-        CarDto carDtoCreateTest = new CarDto(
-                null,
-                null,
-                "Volvo",
-                "S3 turbo",
-                2022,
-                12000.0,
-                Car.Type.SPORT_CAR,
-                new CarShowroomDto(1L, "KARTEK-AUTO")
-        );
-
-        testCar = carService.save(carDtoCreateTest);
-    }
-    @AfterEach
-    void clean(){
-        carRepository.deleteById(testCar.getId());
-    }
+    @Qualifier("carTestBean")
+    private Car carTest;
     @Test
     @Order(1)
     @DisplayName("Getting car by id throw CarNotFoundException")
@@ -52,7 +36,7 @@ class CarServiceTest {
     @Order(2)
     @DisplayName("Getting car by id")
     void getCarById() {
-        Assertions.assertNotNull(carService.getById(testCar.getId()));
+        Assertions.assertNotNull(carService.getById(carTest.getId()));
     }
 
     @Test
@@ -64,25 +48,30 @@ class CarServiceTest {
 
     @Test
     @Order(4)
-    @DisplayName("Delete car by id")
-    void deleteCarById(){
-        carService.deleteById(testCar.getId());
-    }
-
-    @Test
-    @Order(5)
-    @DisplayName("Delete car by id throw MachinesOutOfStockException")
-    void deleteCarByIdMachinesOutOfStockException(){
-        carService.deleteById(testCar.getId());
-        Assertions.assertThrows(MachinesOutOfStockException.class,
-                ()->carService.deleteById(testCar.getId()));
-    }
-    @Test
-    @Order(6)
     @DisplayName("Update car by id")
     @Transactional
     void updateCarById(){
-        carService.updateById(testCar.getId(),13000.0);
-        Assertions.assertEquals(13000.0,testCar.getPrice());
+        Assertions.assertEquals(
+                13000.0,
+                        carService.updateById(carTest.getId(),13000.0).price());
     }
+    @Test
+    @Order(5)
+    @DisplayName("Delete car by id")
+    void deleteCarById(){
+        int initialQuantity = carTest.getLeftInStock();
+        int afterRemoval = initialQuantity-1;
+        carService.deleteById(carTest.getId());
+        assertEquals(afterRemoval,
+                carService.getById(carTest.getId()).leftInStock());
+    }
+
+    @Test
+    @Order(6)
+    @DisplayName("Delete car by id throw MachinesOutOfStockException")
+    void deleteCarByIdMachinesOutOfStockException(){
+        Assertions.assertThrows(MachinesOutOfStockException.class,
+                ()->carService.deleteById(carTest.getId()));
+    }
+
 }
