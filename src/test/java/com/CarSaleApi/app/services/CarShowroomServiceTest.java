b1462d533f17bca56d26c7760e73a40cd148f989
Никита <128186146+n1kkita@users.noreply.github.com>
2023-09-13 19:02:20 +0300

    added test for CarShowroomService

--- src/test/java/com/CarSaleApi/app/services/CarShowroomServiceTest.java
+++ src/test/java/com/CarSaleApi/app/services/CarShowroomServiceTest.java
@@ -1,37 +1,28 @@
 package com.CarSaleApi.app.services;
 
-import com.CarSaleApi.app.dto.CarShowroomDto;
 import com.CarSaleApi.app.entity.CarShowroom;
-import com.CarSaleApi.app.repositories.CarShowroomRepository;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class CarShowroomServiceTest {
     @Autowired
     private CarShowroomService carShowroomService;
-    @Autowired
-    private CarShowroomRepository carShowroomRepository;
-    private CarShowroom carShowroomTest;
-    @BeforeEach
-    void setup(){
-        carShowroomTest = carShowroomService.save(
-                new CarShowroomDto(null,"TestCarShowroom")
-        );
-    }
 
-    @AfterEach
-    void clean(){
-        carShowroomRepository.deleteById(carShowroomTest.getId());
-    }
+    @Autowired
+    @Qualifier(value = "carShowroomTestBean")
+    private CarShowroom carShowroomTest;
 
     @Test
     @Order(1)
     @DisplayName("Get carshowroom by id")
     void getCarShowroomById(){
-        System.out.println(carShowroomTest.getId());
         assertNotNull(carShowroomService.getById(carShowroomTest.getId()));
     }
 }
